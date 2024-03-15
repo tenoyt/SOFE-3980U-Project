f361a2992b5a27131ea3ac33e2bb952808530a65
Tenoy <tenoy.thankachan@ontariotechu.net>
2024-03-15 12:56:48 -0400

    s

---
+++ src/main/java/com/ontariotechu/ticketbookingapplication/TicketBookingApplication.java
@@ -0,0 +1,15 @@
+package com.ontariotechu.ticketbookingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingApplication.class, args);
+		int ch;
+		System.out.println(S);
+	}
+
+}
---
+++ src/test/java/com/ontariotechu/ticketbookingapplication/TicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ontariotechu.ticketbookingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
